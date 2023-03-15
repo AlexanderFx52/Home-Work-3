c61e05d6a88c8e55dbfc5e744c5ecc93470ffa68
Alexander <fiireex@yandex.ru>
2023-03-15 18:03:22 +0300

    Done Home Work

---
+++ TaskThree/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите число до которого будем куб возводить");
+int a = int.Parse(Console.ReadLine()!);
+    for(int i = 1; i <= a; i++)
+        Console.WriteLine($"{i} в кубе = {i*i*i}");
